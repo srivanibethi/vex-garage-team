003e21d07c2d725c7e94447a920d703e7f4218d4
Coding <bethi.srivani@gmail.com>
2025-03-08 09:45:48 -0800

    Update robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,22 +7,22 @@ using code = vision::code;
 brain Brain;
 controller Controller;
 
-motor FL = motor(PORT16, ratio6_1, true);
-motor ML = motor(PORT17, ratio6_1, true);
-motor BL = motor(PORT19, ratio6_1, false);
+motor FL = motor(PORT4, ratio6_1, true);
+motor ML = motor(PORT1, ratio6_1, true);
+motor BL = motor(PORT8, ratio6_1, false);
 motor_group LeftDrive = motor_group(FL, ML, BL);
-motor FR = motor(PORT11, ratio6_1, false);
-motor MR = motor(PORT13, ratio6_1, false);
-motor BR = motor(PORT12, ratio6_1, true);
+motor FR = motor(PORT3, ratio6_1, false);
+motor MR = motor(PORT7, ratio6_1, false);
+motor BR = motor(PORT9, ratio6_1, true);
 motor_group RightDrive = motor_group(FR, MR, BR);
 
-digital_out Arm = digital_out(Brain.ThreeWirePort.H);
-digital_out Clamp = digital_out(Brain.ThreeWirePort.G);
+digital_out D = digital_out(Brain.ThreeWirePort.H);
+digital_out P = digital_out(Brain.ThreeWirePort.G);
 
-motor Intake1 = motor(PORT15, ratio6_1, true);
-motor Intake2 = motor(PORT18, ratio6_1, false);
+motor Intake1 = motor(PORT5, ratio6_1, true);
+motor Intake2 = motor(PORT2, ratio6_1, false);
 motor_group Intake = motor_group(Intake1,Intake2);
 
-inertial Inertial = inertial(PORT14);
+inertial Inertial = inertial(PORT6);
 
 void vexcodeInit(void);
