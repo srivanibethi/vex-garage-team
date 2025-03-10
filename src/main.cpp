9016ac05d974f307892b14caa20c7f022c171bf6
Coding <bethi.srivani@gmail.com>
2025-03-09 21:56:26 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "vex.h"
-#include "driver-contro.hpp"
+#include "driver-control.hpp"
 #include "robot-config.hpp"
 #include "auton.h"
 using namespace vex;
@@ -31,7 +31,7 @@ void autonomous(void) {
   // AutonSkills(); // Uncomment this line to run Skills autonomous
   
   // Choose one of these autonomous functions:
-  TestPID();    // PID-based autonomous (more accurate)
+  //   TestPID();    // PID-based autonomous (more accurate)
   // CustomAuton(); // Alternative time-based autonomous (simpler but less accurate)
   
   inauton = false;
